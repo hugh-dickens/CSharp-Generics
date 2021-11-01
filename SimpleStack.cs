b8ca0fcd8153d0d311832913549555fb693b7889
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-11-01 15:41:07 +0000

    Understanding the need for Generics

---
+++ SimpleStack.cs
@@ -0,0 +1,17 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack<T> // this is a generic type parameter
+    {
+        private readonly T[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new T[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(T item) => _items[++_currentIndex] = item; // increments before statement
+
+        public T Pop() => _items[_currentIndex--];
+    }
+
+}
